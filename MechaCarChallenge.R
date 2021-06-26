8ad7ac36dd63952a72c9d37e721fc6339b356836
ElaineNg94 <79742633+ElaineNg94@users.noreply.github.com>
2021-06-26 03:03:27 -0700

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -19,33 +19,14 @@ SusCoil <- read.csv(file = 'Suspension_Coil.csv', check.names = F, stringsAsFact
 #Write an RScript that creates a total_summary dataframe using the summarize() function to get the mean, median, variance, and standard deviation of the suspension coil's PSI column.(15.2.5)
 total_summary <- SusCoil %>% summarize("Mean"=mean(PSI),"Median"=median(PSI), "Variance"=var(PSI), "SD"=sd(PSI))
 
-#summarize(SusCoil, var(PSI))
-#total_summary <- data.frame("mean" = summarize(SusCoil, mean(PSI)), "median" = summarize(SusCoil, median(PSI)), "variance"= summarize(SusCoil, var(PSI)), "standard deviation" = summarize(SusCoil, sd(PSI)))
-#total_summary %>% 
-#rename(
-#mean.PSI = Mean,
-#median.PSI = Median,
-#var.PSI = Variance,
-#sd.PSI = SD
-#)
-#head(total_summary)
-
 #Write an RScript that creates a lot_summary dataframe using the group_by() and the summarize() functions to group each manufacturing lot by the mean, median, variance, and standard deviation of the suspension coil's PSI column.(15.2.5)
 lot_summary <- SusCoil %>%group_by(Manufacturing_Lot)%>% summarize("Mean"=mean(PSI),"Median"=median(PSI), "Variance"=var(PSI), "SD"=sd(PSI))
 
 #Deliverable 3
 #Write an RScript using the t.test() function to determine if the PSI across all manufacturing lots is statistically different from the population mean of 1,500 pounds per square inch.
 t.test(SusCoil$PSI,mu=1500)
-#SusCoil$Manufacturing_Lot
 
 #Write three more RScripts in your MechaCarChallenge.RScript using the t.test() function and its subset() argument to determine if the PSI for each manufacturing lot is statistically different from the population mean of 1,500 pounds per square inch.(15.3.4)
 t.test(subset(SusCoil, Manufacturing_Lot == "Lot1")$PSI, mu = 1500)
 t.test(subset(SusCoil, Manufacturing_Lot == "Lot2")$PSI, mu = 1500)
 t.test(subset(SusCoil, Manufacturing_Lot == "Lot3")$PSI, mu = 1500)
-
-#Notes: 15.3.4
-#mpg_summary <- subset(mpg,manufacturer=="toyota") %>% group_by(cyl) %>% summarize(Mean_Hwy=mean(hwy), .groups = 'keep') #create summary table
-t.test(subset(SusCoil$PSI, SusCoil$Manufacturing_Lot == "Lot 1"), mu=1500)
-       #%>% group_by(cyl) %>% summarize(Mean_Hwy=mean(hwy), .groups = 'keep')
-#^t.test FUNCTION, subset ARGUMENT to determine SusCoil PSI for each SusCoil manufacturing_lot, mean 1500
-#^ $ refers where to grab in the file
